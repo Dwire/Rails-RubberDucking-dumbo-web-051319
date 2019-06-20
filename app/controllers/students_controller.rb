438cef4d06581a7d9e033331c8c1c9615adeb9b5
Dwire <gjdwyer8@gmail.com>
2019-06-20 16:11:41 -0400

    ruber duck review

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,30 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+
+    if student.valid?
+      redirect_to student_path(student)
+    else
+      flash[:errors] = student.errors.full_messages
+      flash[:note] = "fun note to user"
+      redirect_to new_student_path
+    end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
 end
