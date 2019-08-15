38886f6c4840e879b3e46e256506a2c20408eb13
Dwire <gjdwyer8@gmail.com>
2019-08-15 18:54:06 -0400

    Finished up Duck edit funcionality with a validation + error messages

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -10,6 +10,8 @@ class DucksController < ApplicationController
 
   def new
     @duck = Duck.new
+    #pass and instance variable instead of writing Student.all in the collection_select (MVC!!)
+    @students = Student.all
   end
 
   def create
@@ -25,20 +27,25 @@ class DucksController < ApplicationController
 
   def edit
     @duck = Duck.find(params[:id])
+    @students = Student.all
   end
 
   def update
     @duck = Duck.find(params[:id])
-    @duck.update(duck_params)
-
-    redirect_to duck_path(@duck)
-    # redirect_to @duck
+    
+    # update errors are the same as create but .update returns a boolean so we don't need .valid? or .save in our if statment
+    if @duck.update(duck_params)
+      redirect_to @duck
+    else 
+      flash[:errors] = @duck.errors.full_messages
+      redirect_to edit_duck_path(@duck)
+    end 
   end 
 
   private
 
   def duck_params
-    params.require(:duck).permit(:name, :description)
+    params.require(:duck).permit(:name, :description, :student_id)
   end
   
 end
