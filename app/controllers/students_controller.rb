92d350636928f7f21cb7fef5dded5a22ec9e60bd
Dwire <gjdwyer8@gmail.com>
2019-08-15 18:16:33 -0400

    review code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,35 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @ducks = @student.ducks
+  end
+
+  def new
+    @student = Student.new
+  end
+  
+  def create
+    @student = Student.create(student_params)
+      if @student.valid?
+        redirect_to student_path(@student)
+      else
+        # If we redirect save errors to FLASH_HASH
+          flash[:errors] = @student.errors.full_messages
+          redirect_to new_student_path
+
+        # If we render save errors to instance variable
+          # @errors = @student.errors.full_messages
+          # render :new
+      end
+  end
+  
+  private 
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+  
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  has_many :ducks
+
+  validates :name, presence: true
+  # validates :mod, numericality: {minimum: 1, maximum: 5}
+  validates :mod, inclusion: 1..5
 end
